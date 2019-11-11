23faf0cee99f1b32b686ad4f8075605a138350e0
Naya  Willis <greedybrain18@gmail.com>
2019-11-11 16:10:53 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -2,3 +2,4 @@
 
 -- It should create a table in a new table in the db/ directory.
 CREATE TABLE IF NOT EXISTS pokemon(id INTEGER PRIMARY KEY, name TEXT, type TEXT);
+https://learn.co/tracks/online-software-engineering-structured/orms-and-activerecord/orms/pokemon-scraper-lab#
